46d236653dd21fa76312bbb0757708ca8b84e383
rxlabz <dev@rxlabz.com>
2017-04-17 23:51:15 +0200

    example1 refacto

--- lib/drag_drop_basics.dart
+++ lib/drag_drop_basics.dart
@@ -10,38 +10,39 @@ class DragBox extends StatefulWidget {
 }
 
 class _DragBoxState extends State<DragBox> {
-  String selection ;
+  String selection;
 
   @override
   Widget build(BuildContext context) {
     return new Stack(children: [
-      new DraggableText('A', new Offset(20.0, 100.0)),
-      new DraggableText('B', new Offset(140.0, 100.0)),
-      new Positioned(
-          left: 40.0,
-          bottom: 40.0,
-          child: new DragTarget(
-              builder: (BuildContext context, List<dynamic> accepted,
-                  List<dynamic> rejected) {
-                final hovered = accepted.length > 0;
-                return new Container(
-                    width: 200.0,
-                    height: 200.0,
-                    decoration: new BoxDecoration(
-                        backgroundColor: hovered
-                            ? Colors.cyan.shade100
-                            : Colors.grey.shade200,
-                        border: new Border.all(
-                            width: 2.0,
-                            color: hovered ? Colors.cyan : Colors.grey)),
-                    child: new Center(child: new Text(selection ?? 'Drop here' )));
-              },
-              onWillAccept: (value) => selection == null,
-              onAccept: (value) => setState(() {
-                    selection = value;
-                  }))),
+      getTarget(),
+      new DraggableText('A', new Offset(20.0, 60.0)),
+      new DraggableText('B', new Offset(180.0, 60.0)),
     ]);
   }
+
+  getTarget() => new Positioned(
+    left: 40.0,
+    bottom: 40.0,
+    child: new DragTarget(
+      builder: (BuildContext context, List<dynamic> accepted,
+        List<dynamic> rejected) {
+        final hovered = accepted.length > 0;
+        return new Container(
+          width: 200.0,
+          height: 200.0,
+          decoration: new BoxDecoration(
+            backgroundColor:
+            hovered ? Colors.cyan.shade100 : Colors.grey.shade200,
+            border: new Border.all(
+              width: 2.0,
+              color: hovered ? Colors.cyan : Colors.grey)),
+          child: new Center(child: new Text(selection ?? 'Drop here')));
+      },
+      onWillAccept: (value) => selection == null,
+      onAccept: (value) => setState(() {
+        selection = value;
+      })));
 }
 
 class DraggableText extends StatefulWidget {
@@ -65,20 +66,20 @@ class _DraggableTextState extends State<DraggableText> {
 
   @override
   Widget build(BuildContext context) {
-    final item = new LabelBox(size: new Size.square(150.0), label: widget.text);
+    final item = new LabelBox(size: new Size.square(100.0), label: widget.text);
     final avatar = new LabelBox(
-        size: new Size.square(150.0), label: widget.text, opacity: 0.4);
+      size: new Size.square(150.0), label: widget.text, opacity: 0.4);
     final draggable = new Draggable(
-        data: widget.text,
-        feedback: avatar,
-        child: item,
-        childWhenDragging: new Opacity(opacity: 0.0, child: item),
-        onDraggableCanceled: (velocity, offset) {
-          print('_DragBoxState.build -> offset ${offset}');
-          setState(() => position = offset);
-        });
+      data: widget.text,
+      feedback: avatar,
+      child: item,
+      childWhenDragging: new Opacity(opacity: 0.0, child: item),
+      onDraggableCanceled: (velocity, offset) {
+        print('_DragBoxState.build -> offset ${offset}');
+        setState(() => position = offset);
+      });
     return new Positioned(
-        left: position.dx, top: position.dy, child: draggable);
+      left: position.dx, top: position.dy, child: draggable);
   }
 }
 
@@ -96,17 +97,17 @@ class LabelBox extends StatefulWidget {
 
 class LabelBoxState extends State<LabelBox> {
   final defaultStyle = new TextStyle(
-      color: Colors.white, decoration: TextDecoration.none, fontSize: 16.0);
+    color: Colors.white, decoration: TextDecoration.none, fontSize: 16.0);
 
   @override
   Widget build(BuildContext context) => new Container(
-        width: widget.size.width,
-        height: widget.size.height,
-        color: Colors.cyan.withOpacity(widget.opacity),
-        child: new Center(
-            child: new Text(
-          widget.label,
-          style: defaultStyle,
-        )),
-      );
+    width: widget.size.width,
+    height: widget.size.height,
+    color: Colors.cyan.withOpacity(widget.opacity),
+    child: new Center(
+      child: new Text(
+        widget.label,
+        style: defaultStyle,
+      )),
+  );
 }
